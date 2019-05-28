ef6aa6643bad75978af4d6001b98fc8c6fa72c91
Graeme <graeme.k.cox@gmail.com>
2019-05-28 19:54:48 -0400

    Add yellow and white masking

--- AutonomousDriving/main.cpp
+++ AutonomousDriving/main.cpp
@@ -14,6 +14,8 @@ using namespace cv;
 
 void displayOriginal(Mat image);
 void displayBrightness(Mat image);
+Mat mask_yw(Mat image);
+
 
 int main(int argc, const char * argv[]) {
     
@@ -25,16 +27,35 @@ int main(int argc, const char * argv[]) {
         return -1;
         
     }
+    // Add yellow and white mask to remove stuff we don't care about
+    Mat mask;
+    mask = mask_yw(image);
 
+    
     Mat dst, cdst;
-    Canny(image, dst, 50, 200, 3);
+    Canny(mask, dst, 50, 150, 3);
+
     printf("Convert color\n");
     cvtColor(dst, cdst, CV_GRAY2BGR);
-    
 
     vector<Vec2f> lines;
     printf("Hough lines\n");
-    HoughLines(dst, lines, 1, CV_PI/180, 100, 0, 0 );
+    // 1.output of edge detector (should be grayscale)
+    
+    /*
+     1. output of edge detector (should be grayscale)
+     2. A vector that will store paramteres( rho, theta) of detected lines)
+     3. rho: Resolution in 'rho' pixels.
+     4. Resolutino of theta in radians. Use 9pi/180) to convert to degree
+     5. Theshold, minimum number of intersections to detect a line
+     6. srn and stn. Defaults to zero
+     */
+    int rho = 2;
+    int thesh = 50;
+    float theta = CV_PI/180;
+    int min_line_len = 50;
+    int max_line_gap = 200;
+    HoughLines(dst, lines, rho, theta, thesh, min_line_len, max_line_gap );
     
     printf("Line stuff\n");
     for( size_t i = 0; i < lines.size(); i++ )
@@ -49,37 +70,31 @@ int main(int argc, const char * argv[]) {
         pt2.y = cvRound(y0 - 1000*(a));
         line( cdst, pt1, pt2, Scalar(0,0,255), 3, CV_AA);
     }
-//    imshow("Source", image);
-//    imshow("Detected Lines", cdst);
+
     displayOriginal(cdst);
-//    Mat gray;
-//    cvtColor(image, gray, COLOR_BGR2GRAY);
-////    Mat gray = cvtColor(image, COLOR_BGR2GRAY);
-//    Mat edges; // for all edges
-//
-//    Canny(gray, edges, 50, 200);
-//    vector<Vec4i> lines;
-//
-//    HoughLines(edges, lines, 1, CV_PI/180, 100, 0, 0);
-////    HoughLines(edges, lines, 1, CV_PI/180, 20, 10, 250);
-//
-//    for (size_t i=0; i<lines.size(); i++) {
-////        Vec4i l = lines[i];
-////        line(src, Point(l[0], l[1]), Point(l[2], l[3]), Scaler(255,0,0), 3, LINE_AA);
-//        float rho = lines[i][0], theta = lines[i][1];
-//        Point pt1, pt2;
-//        double a = cos(theta), b = sin(theta);
-//        double x0 = a*rho, y0 = b*rho;
-//        pt1.x = cvRound(x0 + 1000*(-b));
-//        pt1.y = cvRound(y0 + 1000*(a));
-//        pt2.x = cvRound(x0 - 1000*(-b));
-//        pt2.y = cvRound(y0 - 1000*(a));
-//        line( edges, pt1, pt2, Scalar(0,0,255), 3, CV_AA);
-//    }
-    
     return 0;
 }
 
+Mat mask_yw(Mat image)
+{
+    Mat gray, hsv, mask;
+    cvtColor(image, gray, CV_BGR2GRAY);
+    cvtColor(image, hsv, CV_BGR2HLS);
+    
+    assert(image.type() == CV_8UC3);
+    cv::Scalar lower = cv::Scalar(20, 100, 100); //yellow
+    cv::Scalar upper = cv::Scalar(30, 255, 255);
+
+    Mat mask_w, mask_y;
+    cv::inRange(hsv, lower, upper, mask_y);
+    cv::inRange(gray, 200,255, mask_w);
+    
+    bitwise_or(mask_w, mask_y, mask);
+//    displayOriginal(mask_w);
+//    displayOriginal(mask_y);
+    return mask;
+    
+}
 
 void displayOriginal(Mat image) {
     String windowname = "Original";
