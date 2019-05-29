6eb63f9c92a16c918185134d6ff49254ab014e39
Graeme <graeme.k.cox@gmail.com>
2019-05-28 21:57:02 -0400

    Small code cleanup

--- AutonomousDriving/main.cpp
+++ AutonomousDriving/main.cpp
@@ -15,6 +15,7 @@ using namespace cv;
 void displayOriginal(Mat image);
 void displayBrightness(Mat image);
 Mat mask_yw(Mat image);
+Mat getHoughLines(Mat src);
 
 
 int main(int argc, const char * argv[]) {
@@ -27,50 +28,14 @@ int main(int argc, const char * argv[]) {
         return -1;
         
     }
+    
+    
     // Add yellow and white mask to remove stuff we don't care about
     Mat mask;
     mask = mask_yw(image);
 
-    
-    Mat dst, cdst;
-    Canny(mask, dst, 50, 150, 3);
 
-    printf("Convert color\n");
-    cvtColor(dst, cdst, CV_GRAY2BGR);
-
-    vector<Vec2f> lines;
-    printf("Hough lines\n");
-    // 1.output of edge detector (should be grayscale)
-    
-    /*
-     1. output of edge detector (should be grayscale)
-     2. A vector that will store paramteres( rho, theta) of detected lines)
-     3. rho: Resolution in 'rho' pixels.
-     4. Resolutino of theta in radians. Use 9pi/180) to convert to degree
-     5. Theshold, minimum number of intersections to detect a line
-     6. srn and stn. Defaults to zero
-     */
-    int rho = 2;
-    int thesh = 50;
-    float theta = CV_PI/180;
-    int min_line_len = 50;
-    int max_line_gap = 200;
-    HoughLines(dst, lines, rho, theta, thesh, min_line_len, max_line_gap );
-    
-    printf("Line stuff\n");
-    for( size_t i = 0; i < lines.size(); i++ )
-    {
-        float rho = lines[i][0], theta = lines[i][1];
-        Point pt1, pt2;
-        double a = cos(theta), b = sin(theta);
-        double x0 = a*rho, y0 = b*rho;
-        pt1.x = cvRound(x0 + 1000*(-b));
-        pt1.y = cvRound(y0 + 1000*(a));
-        pt2.x = cvRound(x0 - 1000*(-b));
-        pt2.y = cvRound(y0 - 1000*(a));
-        line( cdst, pt1, pt2, Scalar(0,0,255), 3, CV_AA);
-    }
-
+    Mat cdst = getHoughLines(mask);
     displayOriginal(cdst);
     return 0;
 }
@@ -142,4 +107,50 @@ void displayBrightness(Mat image) {
     waitKey(0);
     destroyWindow(windowname);
     
+}
+
+
+Mat getHoughLines(Mat src)
+{
+    Mat dst, cdst;
+    Canny(src, dst, 50, 150, 3);
+    
+    printf("Convert color\n");
+    cvtColor(dst, cdst, CV_GRAY2BGR);
+    
+    vector<Vec2f> lines;
+    printf("Hough lines\n");
+    // 1.output of edge detector (should be grayscale)
+    
+    /*
+     1. output of edge detector (should be grayscale)
+     2. A vector that will store paramteres( rho, theta) of detected lines)
+     3. rho: Resolution in 'rho' pixels.
+     4. Resolutino of theta in radians. Use 9pi/180) to convert to degree
+     5. Theshold, minimum number of intersections to detect a line
+     6. srn and stn. Defaults to zero
+     */
+    int rho = 2;
+    int thesh = 40;
+    float theta = CV_PI/180;
+    int min_line_len = 50;
+    int max_line_gap = 200;
+    HoughLines(dst, lines, rho, theta, thesh, min_line_len, max_line_gap );
+    
+    printf("Line stuff\n");
+    for( size_t i = 0; i < lines.size(); i++ )
+    {
+        float rho = lines[i][0], theta = lines[i][1];
+        Point pt1, pt2;
+        double a = cos(theta), b = sin(theta);
+        double x0 = a*rho, y0 = b*rho;
+        pt1.x = cvRound(x0 + 1000*(-b));
+        pt1.y = cvRound(y0 + 1000*(a));
+        pt2.x = cvRound(x0 - 1000*(-b));
+        pt2.y = cvRound(y0 - 1000*(a));
+        line( cdst, pt1, pt2, Scalar(0,0,255), 3, CV_AA);
+    }
+    
+    return cdst;
+
 }
